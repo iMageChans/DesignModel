ebc9e5bfce3fd42816a8e49c1184426bdec38d3f
iMageChans <imagechans@gmail.com>
2020-02-05 00:19:46 +0800

    update

---
+++ Java/collections/src/iterator/DinerMenuIterator.java
@@ -0,0 +1,26 @@
+package iterator;
+
+public class DinerMenuIterator implements Iterator {
+    String[] items;
+    int position = 0;
+
+    public DinerMenuIterator(String[] items) {
+        this.items = items;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (position >= items.length || items[position] == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public Object next() {
+        String menuItem = items[position];
+        position = position + 1;
+        return menuItem;
+    }
+}
---
+++ Java/collections/src/iterator/PancakeHouseMenuIterator.java
@@ -0,0 +1,28 @@
+package iterator;
+
+import java.util.ArrayList;
+
+public class PancakeHouseMenuIterator implements Iterator{
+    ArrayList<String> items;
+    int position = 0;
+
+    public PancakeHouseMenuIterator(ArrayList<String> items) {
+        this.items = items;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (position >= items.size()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public Object next() {
+        String menuItem = (String) items.get(position);
+        position = position + 1;
+        return menuItem;
+    }
+}
