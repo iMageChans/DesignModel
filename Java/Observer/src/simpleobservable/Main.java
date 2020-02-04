ecf35666aab31a7e8138bfe10ea2f5be2ccdb264
iMageChans <imagechans@gmail.com>
2020-02-04 15:41:30 +0800

    update obs

---
+++ Java/Observer/src/simpleobservable/Main.java
@@ -0,0 +1,9 @@
+package simpleobservable;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleSubject simpleSubject = new SimpleSubject();
+        SimpleObserver simpleObserver = new SimpleObserver(simpleSubject);
+        simpleSubject.setValue(80);
+    }
+}
---
+++ Java/Observer/src/simpleobservable/SimpleSubject.java
@@ -0,0 +1,17 @@
+package simpleobservable;
+
+import java.util.Observable;
+
+public class SimpleSubject extends Observable {
+    private int value = 0;
+
+    public void setValue(int value){
+        this.value = value;
+        setChanged();
+        notifyObservers(value);
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+}
