b73815455014ec7174ed08c32909c38b1216d0e9
iMageChans <imagechans@gmail.com>
2020-02-04 17:45:15 +0800

    update decorator

---
+++ Java/decorator/src/starbuzz/Milk.java
@@ -0,0 +1,17 @@
+package starbuzz;
+
+public class Milk extends CondimentDecorator {
+
+    public Milk(Beverage beverage){
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + "Milk";
+    }
+
+    public double cost(){
+        return .10 + beverage.cost();
+    }
+}
---
+++ Java/decorator/src/starbuzz/Soy.java
@@ -0,0 +1,17 @@
+package starbuzz;
+
+public class Soy extends CondimentDecorator {
+
+    public Soy(Beverage beverage){
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Soy";
+    }
+
+    public double cost(){
+        return .15 + beverage.cost();
+    }
+}
