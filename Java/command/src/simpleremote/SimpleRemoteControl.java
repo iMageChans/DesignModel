f63cff468ad0ca4402c91f8345467f592ea3ecfe
iMageChans <imagechans@gmail.com>
2020-02-04 22:18:23 +0800

    update

---
+++ Java/command/src/simpleremote/SimpleRemoteControl.java
@@ -0,0 +1,13 @@
+package simpleremote;
+
+public class SimpleRemoteControl {
+    Command slot;
+
+    public void setCommand(Command command) {
+        slot = command;
+    }
+
+    public void buttonWasPressed() {
+        slot.execute();
+    }
+}
