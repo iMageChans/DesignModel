3dab4c4de9624cc1ceb94351ee737f39293d24c3
iMageChans <imagechans@gmail.com>
2020-02-13 15:57:19 +0800

    update

---
+++ Rust/strategy/src/main.rs
@@ -0,0 +1,6 @@
+use strategy::duck::Duck;
+
+fn main() {
+    let duck = Duck::new();
+    duck.display();
+}
---
+++ Rust/strategy/target/debug/deps/strategy-8f6253786372fc99.d
@@ -0,0 +1,8 @@
+C:\Users\Chans\Documents\DesignModel\Rust\strategy\target\debug\deps\strategy-8f6253786372fc99.rmeta: src\lib.rs src\duck.rs
+
+C:\Users\Chans\Documents\DesignModel\Rust\strategy\target\debug\deps\libstrategy-8f6253786372fc99.rlib: src\lib.rs src\duck.rs
+
+C:\Users\Chans\Documents\DesignModel\Rust\strategy\target\debug\deps\strategy-8f6253786372fc99.d: src\lib.rs src\duck.rs
+
+src\lib.rs:
+src\duck.rs:
